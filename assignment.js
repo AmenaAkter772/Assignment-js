a245e83d0e84c318fc61fc831c467d0ed4eefe8f
AmenaAkter772 <amenavrt@gmail.com>
2020-07-17 19:45:05 +0600

    added everything

---
+++ assignment.js
@@ -0,0 +1,51 @@
+
+// ----feetToMile-----
+
+function feetToMile(feet){
+        var mile = feet / 5280;
+        return mile;
+}
+var result = feetToMile(1000);
+console.log(result); 
+
+
+// ----woodCalculator in cubic---
+
+function woodCalculator(chair, table, khat){
+    var chairCount = chair * 1 * 1728;
+    var tableCount = table * 3 * 1728;
+    var khatCount = khat * 5 * 1728;
+    var totalFurnichure = chairCount + tableCount + khatCount;
+    return totalFurnichure;
+}
+var furnichureResult = woodCalculator(2, 3, 4);
+console.log(furnichureResult);  
+
+
+
+// ----tinyFrient -----
+
+function tinyFriend(array){
+    let minEle = array[0];
+    for(let i=1;i<array.length;i++){
+        if(array[i].length<minEle.length){
+          minEle = array[i];
+        }
+    }
+    return minEle;
+    }
+    var result = tinyFriend(['moni','sarika','onamika','mahejabin','mo','moyna','sonalyborno']);
+    console.log(result); 
+
+
+
+
+    
+     
+   
+
+
+
+
+
+
